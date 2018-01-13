c26e4b1a7515619e8b1113f734dd01d4e18b1879
falehjamal <falehdewe.fj@gmail.com>
2018-01-13 14:48:43 +0700

    add

---
+++ dinus/register.php
@@ -0,0 +1,58 @@
+<link rel="stylesheet" type="text/css" href="dist/css/bootstrap.min.css">
+<link rel="stylesheet" type="text/css" href="style.css">
+<div class="container">
+  
+  <div class="row" id="pwd-container">
+    <div class="col-md-4"></div>
+    
+    <div class="col-md-4">
+      <section class="login-form">
+        <form method="post" action="#" role="login">
+          <!-- <img src="http://i.imgur.com/RcmcLv4.png" class="img-responsive" alt="" /> -->
+          <span >Daftar</span>
+          <hr>
+          <input type="text" name="nama" placeholder="Nama" required class="form-control input-lg"/>
+          <input type="email" name="email" placeholder="Email" required class="form-control input-lg"/>
+          
+          <input type="password" class="form-control input-lg" name="password" placeholder="Password" required="" />
+          <input type="password" class="form-control input-lg" name="ulangipassword" placeholder="Ulangi password" required="" />
+
+          <select name="jk" class="form-control input-lg">
+          	<option value="">Jenis kelamin</option>
+          	<option value="1">Laki-laki</option>
+          	<option value="2">Perempuan</option>
+          </select>
+
+          <input type="number" name="nohp" placeholder="NO HP" class="form-control">
+          
+          
+          <div class="pwstrength_viewport_progress"></div>
+          
+          
+          <button type="submit" name="go" class="btn btn-lg btn-primary btn-block">Sign in</button>
+          <div>
+            <a href="#">Create account</a> or <a href="#">reset password</a>
+          </div>
+          
+        </form>
+        
+        <div class="form-links">
+          <a href="https://www.tutorialpedia.net"></a>
+        </div>
+      </section>  
+      </div>
+      
+      <div class="col-md-4"></div>
+      
+
+  </div>
+  
+  <!-- <p>
+    <a href="http://validator.w3.org/check?uri=http%3A%2F%2Fbootsnipp.com%2Fiframe%2FW00op" target="_blank"><small>HTML</small><sup>5</sup></a>
+    <br>
+    <br>
+    
+  </p>      -->
+  
+  
+</div>
