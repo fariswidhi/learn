9fc4889e89ea9283a93152af931542acdae5915b
falehjamal <falehdewe.fj@gmail.com>
2018-01-16 12:02:38 +0700

    dinus

--- dinus/index.php
+++ dinus/index.php
@@ -1,99 +1,46 @@
+<link rel="stylesheet" type="text/css" href="dist/css/bootstrap.min.css">
+<link rel="stylesheet" type="text/css" href="style.css">
+<div class="container">
+  
+  <div class="row" id="pwd-container">
+    <div class="col-md-4"></div>
+    
+    <div class="col-md-4">
+      <section class="login-form">
+        <form method="post" action="#" role="login">
+          <img src="http://i.imgur.com/RcmcLv4.png" class="img-responsive" alt="" />
+          <input type="email" name="email" placeholder="Email" required class="form-control input-lg"/>
+          
+          <input type="password" class="form-control input-lg" id="password" placeholder="Password" required="" />
+          
+          
+          <div class="pwstrength_viewport_progress"></div>
+          
+          
+          <button type="submit" name="go" class="btn btn-lg btn-primary btn-block">Sign in</button>
+          <div>
+            <a href="#">Create account</a> or <a href="#">reset password</a>
+          </div>
+          
+        </form>
+        
+        <div class="form-links">
+          <a href="https://www.tutorialpedia.net"></a>
+        </div>
+      </section>  
+      </div>
+      
+      <div class="col-md-4"></div>
+      
 
-<!doctype html>
-<html lang="en">
-  <head>
-    <meta charset="utf-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
-    <meta name="description" content="">
-    <meta name="author" content="">
-    <link rel="icon" href="../../../../favicon.ico">
-
-    <title>Login</title>
-
-    <!-- Bootstrap core CSS -->
-<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-beta.3/css/bootstrap.min.css" integrity="sha384-Zug+QiDoJOrZ5t4lssLdxGhVrurbmBWopoEl+M6BdEfwnCJZtKxi1KgxUyJq13dy" crossorigin="anonymous">
-
-  </head>
-
-  <body>
-<div class="pattern">
+  </div>
+  
+  <!-- <p>
+    <a href="http://validator.w3.org/check?uri=http%3A%2F%2Fbootsnipp.com%2Fiframe%2FW00op" target="_blank"><small>HTML</small><sup>5</sup></a>
+    <br>
+    <br>
+    
+  </p>      -->
+  
   
 </div>
-    <div class="container">
-
-      <div class="row">
-        <div class="col-lg-6" style="float: none;margin:0 auto;">
-        <div class="card">
-          
-          <div class="card-body">
-            <form class="form-signin">
-            
-        <h2 class="form-signin-heading">Login</h2>
-        <label>Email</label>
-        <input type="email" autocomplete="off" id="inputEmail" class="form-control" placeholder="Email address" required autofocus>
-        <br>
-        <label>Password</label>
-        <label for="inputPassword" class="sr-only">Password</label>
-        <input type="password"  autocomplete="off" id="inputPassword" class="form-control" placeholder="Password" required>
-        <br>
-        <button class="btn btn-lg btn-primary btn-block" type="submit">Login</button>
-      </form>
-          </div>
-        </div>
-        </div>
-      </div>
-
-    </div> 
-
-    <style type="text/css">
-      body {
-
-  padding-bottom: 40px;
-  background-color: #eee;
-}
-.container{
-  padding-top: 50px;
-}
-.pattern{
-  height: 40%;
-  float: left;
-  position: absolute;
-  background: red;
-  width: 100%;
-  top: 0;
-}
-.form-signin {
-  max-width: 80%;
-  padding: 15px;
-  margin: 0 auto;
-}
-.form-signin .form-signin-heading,
-.form-signin .checkbox {
-  margin-bottom: 10px;
-}
-.form-signin .checkbox {
-  font-weight: 400;
-}
-.form-signin .form-control {
-  position: relative;
-  box-sizing: border-box;
-  height: auto;
-  padding: 10px;
-  font-size: 16px;
-}
-.form-signin .form-control:focus {
-  z-index: 2;
-}
-.form-signin input[type="email"] {
-  margin-bottom: -1px;
-  border-bottom-right-radius: 0;
-  border-bottom-left-radius: 0;
-}
-.form-signin input[type="password"] {
-  margin-bottom: 10px;
-  border-top-left-radius: 0;
-  border-top-right-radius: 0;
-}
-    </style>
-  </body>
-</html>
