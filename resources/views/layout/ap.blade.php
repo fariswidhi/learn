4e9bff22f7106a396bd0ace05d2d501c867febc7
= <fariswidhiarta123@gmail.com>
2018-01-19 13:07:09 +0700

    add

--- resources/views/layout/ap.blade.php
+++ resources/views/layout/ap.blade.php
@@ -5,6 +5,8 @@
     <link rel="stylesheet" type="text/css" href="{{ asset('css/bootstrap.min.css') }}">
   <link rel="stylesheet" type="text/css" href="{{ asset('css/styleberanda.css') }}">
     <link rel="stylesheet" type="text/css" href="{{ asset('font-awesome/css/font-awesome.min.css') }}">
+<meta name="viewport" content="width=device-width, initial-scale=1.0">
+
 </head>
 <style type="text/css">
 
